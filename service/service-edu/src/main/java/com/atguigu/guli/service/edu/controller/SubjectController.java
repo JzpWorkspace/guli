42860db6c5e3b4fc73e844278fd0a56d76af980a
焦泽鹏 <jzp.workspace@foxmail.com>
2020-02-21 22:54:40 +0800

    1.完成excel读取并写入数据库操作 2.完成列表查询组件

--- service/service-edu/src/main/java/com/atguigu/guli/service/edu/controller/SubjectController.java
+++ service/service-edu/src/main/java/com/atguigu/guli/service/edu/controller/SubjectController.java
@@ -1,9 +1,23 @@
 package com.atguigu.guli.service.edu.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.atguigu.guli.common.base.result.R;
+import com.atguigu.guli.common.base.result.ResultCodeEnum;
+import com.atguigu.guli.common.base.util.ExceptionUtils;
+import com.atguigu.guli.service.base.exception.GuLiException;
+import com.atguigu.guli.service.edu.entity.vo.SubjectVo;
+import com.atguigu.guli.service.edu.service.SubjectService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.InputStream;
+import java.util.List;
 
 /**
  * <p>
@@ -13,9 +27,38 @@ import org.springframework.web.bind.annotation.RestController;
  * @author Jzp
  * @since 2020-02-12
  */
-@RestController
-@RequestMapping("/edu/subject")
-public class SubjectController {
 
+@CrossOrigin
+@Api(description = "课程类别信息")
+@RestController
+@RequestMapping("/admin/edu/subject")
+@Slf4j
+public class SubjectController {
+    @Autowired
+    private SubjectService subjectService;
+
+    @ApiOperation(value = "嵌套数据列表")
+    @GetMapping("nested-list")
+    public R nestedList(){
+        List<SubjectVo> subjectVoList = subjectService.nestedList();
+        return R.ok().data("items", subjectVoList);
+    }
+
+    @ApiOperation(value = "Excel导入课程类别信息")
+    @PostMapping("import")
+    public R batchImport(
+            @ApiParam(name = "file",value = "Excel文件",required = true)
+            @RequestParam("file")MultipartFile file
+            ){
+        try {
+            InputStream inputStream = file.getInputStream();
+            subjectService.batchImport(inputStream);
+            return R.ok().message("文件批量上传成功！");
+        } catch (Exception e){
+            log.error(ExceptionUtils.getMessage(e));
+            throw new GuLiException(ResultCodeEnum.FILE_UPLOAD_ERROR);
+        }
+
+    }
 }
 
